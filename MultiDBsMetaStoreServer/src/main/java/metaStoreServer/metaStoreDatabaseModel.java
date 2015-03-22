6474b1d746ef3517f86d0ab3c8991a4a1fb090e1
yuz82 <yuz82@pitt.edu>
2015-03-22 01:35:33 -0400

    DB

--- MultiDBsMetaStoreServer/src/main/java/metaStoreServer/metaStoreDatabaseModel.java
+++ MultiDBsMetaStoreServer/src/main/java/metaStoreServer/metaStoreDatabaseModel.java
@@ -1,20 +1,24 @@
 package metaStoreServer;
 
 public class metaStoreDatabaseModel {
+	private int id;
+	private String database;
 	
-
-		private String database;
-		
-	    public String getDatabase() {
-	        return database;
-	    }
-
-	    public void setDatabase(String database) {
-	        this.database = database;
-	    }
-		
-		public metaStoreDatabaseModel(final String database){
-			this.setDatabase(database);
-
-		}
+	public metaStoreDatabaseModel(final int id, final String database){
+		this.setId(id);
+		this.setDatabase(database);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDatabase() {
+		return database;
+	}
+	public void setDatabase(String database) {
+		this.database = database;
+	}
 }
