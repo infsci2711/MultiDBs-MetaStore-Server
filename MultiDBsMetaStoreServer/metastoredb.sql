d0c5ea7467975710dee2a8f50f154628662255a5
sapphirew <wanghao@Haos-MacBook-Pro.local>
2015-03-24 13:47:45 -0400

    group1 sql

---
+++ MultiDBsMetaStoreServer/metastoredb.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 24, 2015 at 06:45 PM
+-- Server version: 5.5.38
+-- PHP Version: 5.5.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+CREATE DATABASE  IF NOT EXISTS `group1` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `group1`;
+--
+-- Database: `group1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `metaStore`
+--
+
+CREATE TABLE `metaStore` (
+`ID` int(11) NOT NULL,
+  `DBtype` varchar(45) DEFAULT NULL,
+  `IPAddress` varchar(45) DEFAULT NULL,
+  `port` int(11) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `DBname` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `metaStore`
+--
+
+INSERT INTO `metaStore` (`ID`, `DBtype`, `IPAddress`, `port`, `username`, `password`, `DBname`) VALUES
+(1, 'MySQL', '1.2.3.4', 80, 'hao', 'hao', 'metastore'),
+(2, 'MySQL', '1.2.3.4', 80, 'hao', 'hao', 'metastore'),
+(3, 'MySQL', '1.2.3.4', 0, 'hao', 'hao', 'metastore'),
+(4, 'MySQL', '1.2.3.4', 0, 'hao', 'hao', 'store'),
+(5, '1', '1', 1, '1', '1', '1'),
+(6, '1', '1', 1, '1', '1', '1'),
+(7, '2', '2', 2, '2', '2', '2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `metaStore`
+--
+ALTER TABLE `metaStore`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `metaStore`
+--
+ALTER TABLE `metaStore`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
