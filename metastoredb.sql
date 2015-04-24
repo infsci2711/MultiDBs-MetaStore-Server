66cdfdcf68ba563949b8f069b42572cc0e762c1b
EvgenyKarataev <karataev.evgeny@gmail.com>
2015-04-24 13:39:12 -0500

    fixing mysql error

--- metastoredb.sql
+++ metastoredb.sql
@@ -24,7 +24,7 @@ CREATE DATABASE  IF NOT EXISTS `metastore` /*!40100 DEFAULT CHARACTER SET latin1
 USE `metastore`;
 
 CREATE TABLE `datasources` (
-`ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `DBtype` varchar(45) DEFAULT NULL,
   `IPAddress` varchar(45) DEFAULT NULL,
   `port` int(11) DEFAULT NULL,
@@ -32,38 +32,12 @@ CREATE TABLE `datasources` (
   `password` varchar(45) DEFAULT NULL,
   `DBname` varchar(45) DEFAULT NULL,
   `title` varchar(45) DEFAULT NULL,
-  `description` varchar(100) DEFAULT NULL
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 
-
---
--- Dumping data for table `metaStore`
---
-
--- INSERT INTO `metaStore` (`ID`, `DBtype`, `IPAddress`, `port`, `username`, `password`, `DBname`, `title`, `description`) VALUES
--- (1, 'MySQL', 'localhost', 3306, 'root', 'hao', 'group1', 'metastore', 'store metastores');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `metaStore`
---
-ALTER TABLE `datasources`
- ADD PRIMARY KEY (`ID`);
 
 CREATE USER 'metastoreUser'@'localhost' IDENTIFIED BY 'metastoreUser';
 grant all privileges on `metastore`.* to 'metastoreUser'@'localhost';
 
 CREATE USER 'metastoreUser'@'%' IDENTIFIED BY 'metastoreUser';
 grant all privileges on `metastore`.* to 'metastoreUser'@'%';
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `metaStore`
---
-ALTER TABLE `datasources`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
