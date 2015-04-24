e7e70d1562c11aa28f269ea42b88bc3cbd11f222
EvgenyKarataev <karataev.evgeny@gmail.com>
2015-04-24 14:03:49 -0500

    try to get source code now

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,25 @@ echo "running sql script"
 
 mysql -uroot -p$MYSQL_PASSWORD < /home/metastore/metastoredb.sql
 
+echo "creating folder for servers source code"
+
+cd /opt
+mkdir project
+cd project
+
+echo "cloning two git repos"
+
+git clone https://github.com/infsci2711/MultiDBs-Utils.git
+git clone https://github.com/infsci2711/MultiDBs-MetaStore-Server
+
+echo "building utils project"
+
+cd MultiDBs-Utils
+mvn install
+
+echo "building server project"
+
+cd ../MultiDBs-MetaStore-Server
+mvn install
+
 exec "$@"
